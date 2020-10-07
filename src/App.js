fcdea2e7c045f233edf7a4db287cca7047fb656f
emmerz6949 <emmerz6949@gmail.com>
2020-10-07 02:19:27 -0400

    changed the App function to a class and the Table class to a function; moved all of the code except the return statement from Table to App; updated Table to take props.children

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Table from "./components/Table"
 import Wrapper from "./components/Wrapper";
 import Employee from "./components/Employee";
@@ -6,12 +6,37 @@ import SearchForm from "./components/SearchForm";
 import API from "./utils/API";
 import './App.css';
 
-function App() {
-  return (
-    <Wrapper>
-      <Table />
-    </Wrapper>
-  );
+class App extends Component {
+  state = {
+    employees: []
+  }
+
+  componentDidMount() {
+    this.obtainE();
+  }
+
+  obtainE = () => {
+    API.getE()
+      .then(res => this.setState({ employees: res.data.results }))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <Table>
+          {this.state.employees.map(employed => (
+              <Employee 
+                name={{first: employed.name.first, last: employed.name.last}}
+                picture={{medium: employed.picture.medium}}
+                phone={employed.phone}
+                email={employed.email}
+              />
+            ))}
+        </Table>
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
