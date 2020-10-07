36d62ca27ba08af7fc4777980c2de26ab1c45c6c
emmerz6949 <emmerz6949@gmail.com>
2020-10-06 23:44:01 -0400

    added a Table.js file to the components folder; this uses the Employee component and the API utility to create the table of employees

---
+++ src/components/Table.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import Employee from "./Employee";
+import API from "../utils/API";
+
+class Table extends Component {
+    state = {
+        employees: []
+    }
+
+    componentDidMount() {
+        this.obtainE();
+    }
+
+    obtainE = () => {
+        API.getE()
+            .then(res => this.setState({ employees: res.data.results }))
+            .catch(err => console.log(err));
+    }
+
+    render() {
+        return (
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Image</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.employees.map(employed => (
+                        <Employee 
+                            name={{first: employed.name.first, last: employed.name.last}}
+                            picture={{medium: employed.picture.medium}}
+                            phone={employed.phone}
+                            email={employed.email}
+                        />
+                    ))}
+                </tbody>
+            </table>
+        );
+    }
+    
+}
+
+export default Table;
