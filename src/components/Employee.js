eedc4a6b93fd666f3c26054f38206fd8b475ff52
emmerz6949 <emmerz6949@gmail.com>
2020-10-06 23:39:06 -0400

    added the componentss folder and the Employee.js file inside it

---
+++ src/components/Employee.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Employee(props) {
+    let eName = `${props.name.first} ${props.name.last}`;
+    return (
+        <tr>
+            <td><img alt={eName} src={props.picture.medium} /></td>
+            <td className="align-middle">{eName}</td>
+            <td className="align-middle">{props.phone}</td>
+            <td className="align-middle">{props.email}</td>
+        </tr>
+    );
+}
+
+export default Employee;
