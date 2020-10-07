dad79fec06aa521ed368686d77b51ffc613c3ef3
emmerz6949 <emmerz6949@gmail.com>
2020-10-07 02:01:31 -0400

    imported the Employee component, the SearchForm component, and the API utility into App.js, and removed those components and utility from Table.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Table from "./components/Table"
 import Wrapper from "./components/Wrapper";
+import Employee from "./components/Employee";
+import SearchForm from "./components/SearchForm";
+import API from "./utils/API";
 import './App.css';
 
 function App() {
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import Employee from "./Employee";
-import API from "../utils/API";
+
 
 class Table extends Component {
     state = {
